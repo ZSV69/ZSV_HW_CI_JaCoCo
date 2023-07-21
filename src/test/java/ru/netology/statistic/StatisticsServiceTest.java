c23e7bb918761e0400b01443ad5cd3c4bef7f828
Zvyagintsev Sergey <zsv069@gmail.com>
2023-07-21 21:08:16 +0300

    100 percent test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+     public void findMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -16,5 +16,17 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findNewMax() {
+        StatisticsService service = new StatisticsService();
+
+        long [] incomesMaxEqually = {12, 5, 8, 4, 5, 13, 8, 6, 11, 11, 12};
+        long expectedEqually = 13 ;
+        long actualEqually = service.findMax(incomesMaxEqually);
+
+        Assertions.assertEquals(expectedEqually,actualEqually);
+
     }
 }
